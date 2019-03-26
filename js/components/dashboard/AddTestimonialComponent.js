7ef5a22d2f0c22b18eac166f5086cd3c8b5147cf
Joanna Baile <joanna@studentsofferingsupport.org>
2019-03-25 22:59:52 -0400

    cms edits

--- js/components/dashboard/AddTestimonialComponent.js
+++ js/components/dashboard/AddTestimonialComponent.js
@@ -1,7 +1,10 @@
 export default {
     template: `
     <section>
-    <form action="./admin/scripts/add_testimonial.php" method="post" enctype="multipart/form-data">
+    <h4 class="dashboard-title">Add A User</h4>
+    <div class="grid-x">
+        <div class="small-12 large-6 large-offset-3 cell">
+        <form action="./admin/scripts/add_testimonial.php" method="post" enctype="multipart/form-data">
 					
         <div>
         <label>Name</label>
@@ -10,20 +13,23 @@ export default {
             
         <div>
         <label>Upload Image:</label>
-            <input type="file" name="testimonial_file">
+            <input type="file" name="testimonial_file" class="btn">
         </div>
 
         <div>
         <label>Upload Video:</label>
-            <input type="file" name="testimonial_video">
+            <input type="file" name="testimonial_video" class="btn">
         </div>
 
         <div>
         <input type="submit" name="btn_add" class="btn" value="Submit">
-        <a href="#/addtestimonial" class="btn" @click="reload">Start Over</a>
+        <span>or</span>
+        <a href="#/addtestimonial" @click="reload">Start Over</a>        
         </div>
             
     </form>
+        </div>
+    </div>
     </section>
     `,
 
